f4be14969d32191f6f2d8642601a1edfda62fa9f
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-19 12:19:05 -0400

    Resolved merge conflict

--- server/index.js
+++ server/index.js
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
-require('dotenv').config()
-const app = require('./app')
-const db = require('./db/db')
-const PORT = process.env.PORT || 8000
-
-db.syncAndSeed()
-    .then(()=> app.listen(PORT, () => console.log(`\nBackend service running on port ${PORT}\n`)))
-    
-// try {
-//     app.listen(PORT, () => console.log(`\nBackend service running on port ${PORT}\n`))
-// } catch (e) {
-//     console.error(`Failed to load app on port ${PORT}`)
-// }
-=======
 /* load configs */
-require('dotenv').config()
-require('./config/passport-setup')
+require('dotenv').config();
 
 /* app setup */
-const app = require('./app')
-const db = require('./db')
+const app = require('./app');
+const db = require('./db/db');
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 db.sync()
-    .then(app.listen(PORT, () => console.log(`\nApplication running on port ${PORT}\n`)))
-    .catch(e => console.error(`Failed to load app on port ${PORT} with error: ${e}`))
->>>>>>> 8c384d816c3719477da5d5aff3b18d5bb3635b3c
+  .then(
+    app.listen(PORT, () =>
+      console.log(`\nApplication running on port ${PORT}\n`)
+    )
+  )
+  .catch(e =>
+    console.error(`Failed to load app on port ${PORT} with error: ${e}`)
+  );
