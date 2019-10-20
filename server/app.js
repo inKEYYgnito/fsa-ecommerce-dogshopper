6c5afe03d89c4ed116014c23b3577b87fd2f0cd9
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-19 20:01:08 -0400

    Moved dog routes into its own file

--- server/app.js
+++ server/app.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const { Dog } = require('./db/db').models
-app.use(express.json())
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 // API Routes
-app.use('/api/auth', require('./api/auth'))
+app.use('/api/auth', require('./api/auth'));
+app.use('/api/dogs', require('./api/dogs'));
 
-app.use('/', express.static('dist'))
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.use('/', express.static('dist'));
+app.get('/', (req, res, next) =>
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+);
 
 app.use(({ message }, req, res, next) => {
-    res.status(500).send({ message })
-})
+  res.status(500).send({ message });
+});
 
-app.get('/api/dogs', (req,res,next) => {
-    Dog.findAll()
-    .then(dogs => res.send(dogs))
-    .catch(next)
-})
-
-module.exports = app
+module.exports = app;
