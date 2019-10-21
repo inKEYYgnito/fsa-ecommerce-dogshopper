ac7b035da1acc38af7b496c9914291023be0efdc
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-21 01:59:42 -0400

    Load spec helper file prior to running tests

--- server/db/models/Breed.spec.js
+++ server/db/models/Breed.spec.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 chai.use(require('chai-as-promised'));
 const { expect } = chai;
 
-const db = require('../db');
 const Breed = require('./Breed');
 
 describe('Breed Model', () => {
