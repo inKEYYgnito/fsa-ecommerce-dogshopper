b428331bf1c9621a84e53f75c15cd28b7b549fc8
Kristina Cayetano <kma.cayetano@gmail.com>
2019-10-21 00:46:53 -0400

    fixed file structure

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { ROUTE_PATH } from './../commons/constants';
+import { ROUTE_PATH } from './../../commons/constants';
 const { DOGS } = ROUTE_PATH;
 
 const Dogs = ({ dogs }) => (
