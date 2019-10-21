639c5d497cb45649939ef1274b9a0b376a7b99a7
Maria Lapshina <lapmarser@gmail.com>
2019-10-21 16:45:37 -0400

    1st commit enhanced view

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -3,14 +3,17 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { ROUTE_PATH } from './../../commons/constants';
+import './dogs.scss'
 const { DOGS } = ROUTE_PATH;
 
 const Dogs = ({ dogs }) => (
-  <div>
+  <div id='dogs'>
     {dogs.map(dog => (
-      <div key={dog.id}>
-        <Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link> {dog.price}{' '}
-        {dog.description}
+      <div id ='dog' key={dog.id} >
+      <h1><Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link></h1>
+         Price: ${dog.price}
+        <p id ='description'>{dog.description}</p>
+        <button id='button'>Add to cart</button>
       </div>
     ))}
   </div>
