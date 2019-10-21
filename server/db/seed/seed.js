255919d2db4b55a52feaadfdffd32a3f4bbebf74
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-21 02:16:22 -0400

    Added seed file for Breed model

--- server/db/seed/seed.js
+++ server/db/seed/seed.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 const path = require('path');
 
 const connection = require('./../connection');
+const Breed = require('./../models/Breed');
 const Dog = require('./../models/Dog');
 
 const SEED_DATA_FOLDER = path.resolve('./server/db/seed/data');
+const SEED_DATA_FILES = [
+  { model: Dog, file: 'dogs.json' },
+  { model: Breed, file: 'breeds.json' }
+];
 
 const createModelData = (list, Model) => {
   return Promise.all(list.map(el => Model.create(el)));
-};
-
-const getSeedFiles = () => {
-  return fs.readdirSync(SEED_DATA_FOLDER);
 };
 
 const getSeedFileData = file => {
@@ -24,10 +25,9 @@ const getSeedFileData = file => {
 const seed = async () => {
   await connection.sync({ force: true });
 
-  const seedFiles = getSeedFiles();
-  seedFiles.forEach(file => {
+  SEED_DATA_FILES.forEach(async ({ model, file }) => {
     const seedData = getSeedFileData(file);
-    createModelData(seedData, Dog);
+    await createModelData(seedData, model);
   });
 };
 
