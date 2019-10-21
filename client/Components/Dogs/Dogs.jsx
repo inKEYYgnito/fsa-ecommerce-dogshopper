d5db563c47379bf3225ce6e17d9132441d2abf13
Maria Lapshina <lapmarser@gmail.com>
2019-10-21 17:48:18 -0400

    enhanced view in progress-small changes

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -11,8 +11,13 @@ const Dogs = ({ dogs }) => (
     {dogs.map(dog => (
       <div id ='dog' key={dog.id} >
       <h1><Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link></h1>
-         Price: ${dog.price}
-        <p id ='description'>{dog.description}</p>
+        <div id='dogblock'>
+          <div id ='dogtext'>
+          Price: ${dog.price}
+            <p id ='description'>{dog.description}</p>
+          </div>
+            <div id='dogpic'><strong>Pic will be here!</strong></div> 
+        </div>
         <button id='button'>Add to cart</button>
       </div>
     ))}
