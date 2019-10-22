1e3130a3c4f20c09edf2d3481455d5d11d5704de
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-21 23:40:16 -0400

    Add a side bar with breeds dropdown

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import FilterSideBar from './../FilterSideBar';
 import { ROUTE_PATH } from './../../commons/constants';
 const { DOGS } = ROUTE_PATH;
 
 const Dogs = ({ dogs }) => (
   <div>
+    <FilterSideBar />
     {dogs.map(dog => (
       <div key={dog.id}>
         <Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link> {dog.price}{' '}
