bc430dc7cdd463bcd3286e91aeb1d42fee5cd9b4
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-22 00:15:37 -0400

    Filter dog list based on breed

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -7,12 +7,26 @@ import { ROUTE_PATH } from './../../commons/constants';
 const { DOGS } = ROUTE_PATH;
 
 class Dogs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      breedId: 0
+    };
+  }
+
+  updateBreed = breedId => this.setState({ breedId });
+
   render() {
+    const { breedId } = this.state;
     const { dogs } = this.props;
+
+    const dogsToDisplay =
+      breedId === 0 ? dogs : dogs.filter(dog => dog.breedId === breedId);
+
     return (
       <div>
-        <FilterSideBar />
-        {dogs.map(dog => (
+        <FilterSideBar updateBreed={this.updateBreed} breedId={breedId} />
+        {dogsToDisplay.map(dog => (
           <div key={dog.id}>
             <Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link> {dog.price}{' '}
             {dog.description}
--- client/Components/FilterSideBar.jsx
+++ client/Components/FilterSideBar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const FilterSideBar = ({ breeds }) => (
-  <>
-    <select>
-      <option key="">-- Any Breed --</option>
-      {breeds.map(breed => (
-        <option key={breed.id}>{breed.name}</option>
-      ))}
-    </select>
-  </>
-);
+const FilterSideBar = ({ breeds, breedId, updateBreed }) => {
+  const onChange = event => {
+    const breedId = parseInt(event.target.value);
+    updateBreed(breedId);
+  };
+
+  return (
+    <>
+      <select value={breedId} onChange={onChange}>
+        <option key={0} value={0}>
+          -- Any Breed --
+        </option>
+        {breeds.map(breed => (
+          <option key={breed.id} value={breed.id}>
+            {breed.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
 
 const mapStateToProps = ({ breeds }) => ({ breeds });
 
