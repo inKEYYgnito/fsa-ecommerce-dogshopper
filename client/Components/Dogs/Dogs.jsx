2fb8d24886ac0aeaad146b8e3df3c440903e28b3
Maria Lapshina <lapmarser@gmail.com>
2019-10-22 20:01:07 -0400

    small commit

--- client/Components/Dogs/Dogs.jsx
+++ client/Components/Dogs/Dogs.jsx
@@ -7,7 +7,9 @@ import './dogs.scss'
 const { DOGS } = ROUTE_PATH;
 
 const Dogs = ({ dogs }) => (
-  <div id='dogs'>
+    <div>
+      <button>Back to ALL DOGS</button>
+    <div id = 'dogs'>
     {dogs.map(dog => (
       <div id ='dog' key={dog.id} >
       <h1><Link to={`${DOGS}/${dog.id}`}>{dog.name}</Link></h1>
@@ -21,9 +23,10 @@ const Dogs = ({ dogs }) => (
           </div>
             <div id='dogpic'><img src = {dog.imageURL} width="100%" height="100%"/></div> 
         </div>
-        <button id='button'>Add to cart</button>
+        <button id='button'>Add to crate will be here</button>
       </div>
     ))}
+    </div>
   </div>
 );
 
