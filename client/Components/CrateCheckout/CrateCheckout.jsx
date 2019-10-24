9ed9e26805e04d1bc010768f1a0806dc1d2798e9
Charmagne Cayetano <clacayetano@gmail.com>
2019-10-24 09:02:29 -0400

    Added checkout form with email and delivery address fields

--- client/Components/CrateCheckout/CrateCheckout.jsx
+++ client/Components/CrateCheckout/CrateCheckout.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const CrateCheckout = () => <div>Checkout!</div>;
+import './crateCheckout.scss';
 
-export default connect()(CrateCheckout);
+const CrateCheckout = ({ crate }) => {
+  return crate.length ? (
+    <form id="checkout-form">
+      <label>
+        Email
+        <input name="email" />
+      </label>
+      <p>Delivery Address</p>
+      <label>
+        Street
+        <input name="street" />
+      </label>
+      <label>
+        City
+        <input name="city" />
+      </label>
+      <label>
+        State
+        <input name="state" />
+      </label>
+      <label>
+        Country
+        <input name="country" />
+      </label>
+      <label>
+        Zip
+        <input name="zip" />
+      </label>
+      <button>Confirm Checkout</button>
+    </form>
+  ) : (
+    <></>
+  );
+};
+
+const mapStateToProps = ({ crate }) => {
+  return {
+    crate
+  };
+};
+
+export default connect(mapStateToProps)(CrateCheckout);
