6bd53bdd1f7e1dac9e0b211e798593d99f23407c
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-05 03:07:29 -0500

    Link user id of logged in user to created order

--- server/api/orders.js
+++ server/api/orders.js
@@ -5,7 +5,11 @@ const { createOrder } = db.transactions;
 const { Order, OrderItem } = db.models;
 
 router.post('/', async (req, res, next) => {
-  createOrder(req.body)
+  const { order, crate } = req.body;
+  if (req.user) {
+    order.userId = req.user.id;
+  }
+  createOrder({ order, crate })
     .then(orderId =>
       Order.findByPk(orderId, {
         include: [{ model: OrderItem }]
