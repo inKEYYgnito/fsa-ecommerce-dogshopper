d9e93c2b79c7c82e2368d661cfc71df077685aae
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-09 23:32:50 -0500

    Display email address on user profile page

--- client/Components/UserProfile/UserProfile.jsx
+++ client/Components/UserProfile/UserProfile.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import './userProfile.scss'
+import { connect } from 'react-redux';
 
-const UserProfile = () => {
-    return (
-        <div id="user-profile-container">
-            <h1 className="page-title">User Profile</h1>
-            <br />
-            <h2>Coming soon...</h2>
-        </div>
-    );
-}
+import './userProfile.scss';
 
-export default UserProfile;
+const UserProfile = ({ user }) => {
+  return (
+    <div id="user-profile-container">
+      <h1 className="page-title">User Profile</h1>
+      <form id="checkout-form">
+        <label>
+          <h2>Email</h2>
+          <input name="email" value={user.email} disabled />
+        </label>
+        <h2 style={{ marginTop: '1.2vw' }}>Delivery Address</h2>
+        <label>
+          <input name="street" placeholder="Street" title="Street" />
+        </label>
+        <label>
+          <input name="city" placeholder="City" title="City" />
+        </label>
+        <label>
+          <input name="state" placeholder="State" title="State" />
+        </label>
+        <label>
+          <input name="country" placeholder="Country" title="Country" />
+        </label>
+        <label>
+          <input name="zip" placeholder="Zip Code" title="Zip Code" />
+        </label>
+        <button type="submit" className="btn-controls">
+          Checkout
+        </button>
+      </form>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ user }) => {
+  return {
+    user
+  };
+};
+
+export default connect(mapStateToProps)(UserProfile);
