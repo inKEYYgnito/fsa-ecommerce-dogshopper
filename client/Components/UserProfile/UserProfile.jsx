8143fed2e65fabfc9e7d279488f17630d599341e
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-10 01:34:07 -0500

    Update delivery address from profile page

--- client/Components/UserProfile/UserProfile.jsx
+++ client/Components/UserProfile/UserProfile.jsx
@@ -1,45 +1,92 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { updateUser } from '../../store/actions/actions';
 import './userProfile.scss';
 
-const UserProfile = ({ user }) => {
+const UserProfile = ({ user, updateUser }) => {
+  const onSubmit = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const address = {
+      street: form.street.value,
+      city: form.city.value,
+      state: form.state.value,
+      country: form.country.value,
+      zip: form.zip.value
+    };
+    updateUser(address);
+  };
+
+  const address = user.address
+    ? user.address
+    : { street: '', city: '', state: '', country: '', zip: '' };
+
   return (
     <div id="user-profile-container">
       <h1 className="page-title">User Profile</h1>
-      <form id="checkout-form">
+      <form id="checkout-form" onSubmit={onSubmit}>
         <label>
           <h2>Email</h2>
-          <input name="email" value={user.email} disabled />
+          <input type="text" name="email" defaultValue={user.email} disabled />
         </label>
         <h2 style={{ marginTop: '1.2vw' }}>Delivery Address</h2>
         <label>
-          <input name="street" placeholder="Street" title="Street" />
+          <input
+            type="text"
+            name="street"
+            placeholder="Street"
+            defaultValue={address.street}
+          />
         </label>
         <label>
-          <input name="city" placeholder="City" title="City" />
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            defaultValue={address.city}
+          />
         </label>
         <label>
-          <input name="state" placeholder="State" title="State" />
+          <input
+            type="text"
+            name="state"
+            placeholder="State"
+            defaultValue={address.state}
+          />
         </label>
         <label>
-          <input name="country" placeholder="Country" title="Country" />
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            defaultValue={address.country}
+          />
         </label>
         <label>
-          <input name="zip" placeholder="Zip Code" title="Zip Code" />
+          <input
+            type="text"
+            name="zip"
+            placeholder="Zip Code"
+            defaultValue={address.zip}
+          />
         </label>
         <button type="submit" className="btn-controls">
-          Checkout
+          Save
         </button>
       </form>
     </div>
   );
 };
 
-const mapStateToProps = ({ user }) => {
-  return {
-    user
-  };
+const mapStateToProps = ({ user }) => ({ user });
+
+const mapDispatchToProps = {
+  updateUser
 };
 
-export default connect(mapStateToProps)(UserProfile);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserProfile);
--- server/api/user.js
+++ server/api/user.js
@@ -1,7 +1,22 @@
-const router = require('express').Router()
+const router = require('express').Router();
+
+const db = require('./../db/db');
+const { updateAddress } = db.transactions;
+const { User, Address } = db.models;
 
 router.get('/', (req, res) => {
-    res.send(req.user)
-})
+  res.send(req.user);
+});
 
-module.exports = router
+router.put('/', (req, res, next) => {
+  if (req.user) {
+    const user = req.user;
+    const address = req.body;
+    updateAddress({ user, address })
+      .then(() => User.findByPk(user.id, { include: [{ model: Address }] }))
+      .then(user => res.send(user))
+      .catch(next);
+  }
+});
+
+module.exports = router;
